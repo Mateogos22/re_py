8bff5f9acdc515b248634584fc366641944c0b81
golosm <79364017+golosm@users.noreply.github.com>
2022-07-24 20:52:23 +0200

    Create sum-numbers.py
    Sum numbers

---
+++ sum-numbers.py
@@ -0,0 +1,9 @@
+# Enter numbers that you want to sum
+first_num = int(input('Enter the first number: '))
+second_num = int(input('Enter the second number: '))
+
+# Sum numbers
+sum_numbers = first_num + second_num
+
+# Print the result
+print('Your sum result is: ', sum_numbers)
