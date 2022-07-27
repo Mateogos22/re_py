5f5614cfcba9672a13be8cd86f1809a344345102
golosm <golosmateusz@gmail.com>
2022-07-27 19:34:03 +0200

    Porządki w repozytorium

---
+++ nauka/plant.py
@@ -0,0 +1,9 @@
+# This program loves Spathiphyllum and want BIG Spathiphyllum
+plant = input('Enter "Spathiphyllum": ')
+
+if plant == 'spathiphyllum':
+    print('No, I want a big Spathiphyllum!')
+elif plant != 'Spathiphyllum':
+    print('Spathiphyllum! Not', plant,'!')
+else:
+    print('Yes - Spathiphyllum is the best plant ever!')
