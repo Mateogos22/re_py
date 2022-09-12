84994d3bcf0377570237cee03a7842afa70e8f11
golosm <golosmateusz@gmail.com>
2022-09-12 18:43:15 +0200

    Changes in code

---
+++ nauka/75_years.py
@@ -0,0 +1,13 @@
+import datetime
+
+name = input('Enter your name: ')
+
+print("Hello", name,", enter your age and I'll tell you when you turn c years old")
+
+age = int(input('Enter your age: '))
+
+year = datetime.datetime.now().year
+
+age75 = year - age + 75
+
+print(name + ", you will be 75 years old in the year " + str(age75))
